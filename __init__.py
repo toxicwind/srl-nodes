699f9d91a91496dca82d22d2210571e5c5eaf2f0
toxicwind <toxicwind@gmail.com>
2024-05-20 13:52:07 -0600

    Auto-merge and remove duplicates from .gitignore

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ import emoji
 import logging
 import json
 
+
 class AnyType(str):
 
     def __ne__(self, __value: object) -> bool:
@@ -25,15 +26,13 @@ nlp = spacy.load("en_core_web_sm")
 
 # List of words to remove from stop words list
 remove_stop_word_tokens = [
-    'and', 'an', 'the', 'a', 'he', 'she', 'it', 'we', 'you', 'they', 'my',
-    'mine', 'your', 'yours', 'their', 'theirs', 'our', 'ours', 'his', 'her',
+    'and', 'he', 'she', 'it', 'his',
     'its', 'with', 'without', 'before', 'after', 'during', 'around', 'among',
     'between', 'through', 'across', 'behind', 'below', 'beside', 'by', 'over',
     'under', 'into', 'onto', 'upon', 'along', 'above', 'near', 'from',
     'towards', 'to', 'up', 'down', 'in', 'on', 'off', 'out', 'at', 'of', 'not',
     'no', 'none', 'never', 'neither', 'nor', 'only', 'just', 'who', 'whose',
-    'whom', 'which', 'what', 'where', 'when', 'why', 'how', 'whoever',
-    'whichever', 'whenever', 'wherever', 'however', 'every', 'each', 'any',
+    'whom', 'what', 'where', 'any', 'all', 'each', 'every', 'either', 'both',
     'all', 'both', 'another', 'such', 'some', 'few', 'several', 'many', 'much',
     'most', 'more', 'less', 'eight', 'eleven', 'fifteen', 'fifty', 'five',
     'forty', 'four', 'hundred', 'nine', 'one', 'six', 'sixty', 'ten', 'third',
@@ -43,7 +42,7 @@ remove_stop_word_tokens = [
     "get", "give", "go", "here", "hereafter", "hereby", "herein", "hereupon",
     "mostly", "often", "once", "part", "put", "seem", "seemed", "seeming",
     "seems", "take", "together", "top", "toward", "until", "used", "using",
-    "various", "via", "within", "about", "as", "because", "but", "can",
+    "various", "via", "within", "about", "because", "but", "can",
     "cannot", "could", "do", "does", "doing", "done", "even", "for", "if",
     "is", "was", "were", "may", "might", "must", "or", "please", "really",
     "should", "since", "so", "that", "these", "this", "those", "though",
@@ -61,6 +60,7 @@ for word in remove_stop_word_tokens:
 remaining_stop_words = list(nlp.Defaults.stop_words)
 remaining_stop_words.sort()
 print(", ".join(remaining_stop_words))
+
 
 class SrlConditionalInterrupt:
     """Interrupt processing if the boolean input is true. Pass through the other input."""
@@ -266,7 +266,8 @@ class SrlRandomizeAndFormatString:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s')
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
         self.logger.addHandler(stream_handler)
@@ -275,7 +276,8 @@ class SrlRandomizeAndFormatString:
         while True:
             old_processed_string = processed_string
             for word in words_to_replace:
-                processed_string = re.sub(re.escape(word), ",", processed_string)
+                processed_string = re.sub(re.escape(word), ",",
+                                          processed_string)
             if old_processed_string == processed_string:
                 break
         processed_string = re.sub(",{2,}", ",", processed_string)
@@ -333,15 +335,18 @@ class SrlRandomizeAndFormatString:
 
         words_to_replace = [word.lower() for word in words_to_replace]
         words_to_replace.sort(key=len, reverse=True)
-        self.logger.debug("Words to replace: %s", words_to_replace[)
+        self.logger.debug("Words to replace: %s", words_to_replace)
         input_string = self.recursive_replace(input_string, words_to_replace)
         self.logger.debug("After recursive_replace: %s", input_string)
         input_string = input_string.replace(",", "")
-        self.logger.debug("After lowercase and comma replacement: %s", input_string)
+        self.logger.debug("After lowercase and comma replacement: %s",
+                          input_string)
         preserved_periods = re.sub(r"\d+\.(?!\d)", ",", input_string)
-        preserved_periods = re.sub(r"(?<!\d)\.|\.(?!\d)", ",", preserved_periods)
+        preserved_periods = re.sub(r"(?<!\d)\.|\.(?!\d)", ",",
+                                   preserved_periods)
         preserved_periods = re.sub(r"(?<=\s):(?=\s)", ",", preserved_periods)
-        preserved_periods = re.sub(r"(?<=[a-zA-Z]):(?!\d)", " ", preserved_periods)
+        preserved_periods = re.sub(r"(?<=[a-zA-Z]):(?!\d)", " ",
+                                   preserved_periods)
         preserved_periods = preserved_periods.replace(":", ",")
         preserved_periods = preserved_periods.replace("s'", "s")
         preserved_periods = re.sub(r"(?<!\w)'(?!\w)", ",", preserved_periods)
@@ -396,14 +401,21 @@ class SrlRandomizeAndFormatString:
         processed_string = re.sub(r" {2,}", " ", processed_string)
         processed_string = re.sub(r"\s+", " ", processed_string)
         self.logger.debug("After processed_string: %s", processed_string)
-        initial_tokens = [token.strip().lower() for token in processed_string.split(",") if token.strip()]
+        initial_tokens = [
+            token.strip().lower() for token in processed_string.split(",")
+            if token.strip()
+        ]
         self.logger.debug("Initial number of tokens: %s", len(initial_tokens))
 
-        tokens = [self.remove_stop_words(token).strip() for token in initial_tokens]
-        self.logger.debug("Number of tokens after removing stop words: %s", len(tokens))
+        tokens = [
+            self.remove_stop_words(token).strip() for token in initial_tokens
+        ]
+        self.logger.debug("Number of tokens after removing stop words: %s",
+                          len(tokens))
 
         tokens = [token for token in tokens if 3 <= len(token) <= 75]
-        self.logger.debug("Number of tokens after length filtering: %s", len(tokens))
+        self.logger.debug("Number of tokens after length filtering: %s",
+                          len(tokens))
 
         total_tokens = len(tokens)
         selected_tokens = []
@@ -413,7 +425,8 @@ class SrlRandomizeAndFormatString:
             selected_tokens.append(token)
             total_tokens += 1
 
-        self.logger.debug("Number of selected tokens: %s", len(selected_tokens))
+        self.logger.debug("Number of selected tokens: %s",
+                          len(selected_tokens))
 
         final_tokens = tokens + selected_tokens
         random.shuffle(final_tokens)
@@ -424,6 +437,7 @@ class SrlRandomizeAndFormatString:
         self.logger.debug("Final output string: %s", output_string)
 
         return (output_string, )
+
 
 class SrlExtractJson:
     """
@@ -448,6 +462,16 @@ class SrlExtractJson:
                     "min": 0,
                     "max": 999999999
                 }),
+                "min_part_length": ("INT", {
+                    "default": 10,
+                    "min": 0,
+                    "max": 999999999
+                }),
+                "max_part_length": ("INT", {
+                    "default": 10,
+                    "min": 0,
+                    "max": 999999999
+                }),
                 "remove_stop_words": ("BOOLEAN", {
                     "forceInput": False
                 })
@@ -464,7 +488,6 @@ class SrlExtractJson:
     def remove_emoji(self, text):
         return ''.join(char for char in text if not emoji.is_emoji(char))
 
-
     def extract_json_objects(self, text):
         json_pattern = re.compile(r'\{[^\}]*\}', re.DOTALL)
         json_objects = []
@@ -478,24 +501,47 @@ class SrlExtractJson:
         return json_objects
 
     def extract_values(self, json_objects):
-        return [str(item) for obj in json_objects for k, v in obj.items() for item in (v if isinstance(v, list) else [v])]
+        return [
+            str(item) for obj in json_objects for k, v in obj.items()
+            for item in (v if isinstance(v, list) else [v])
+        ]
 
     def extract_and_process_json_from_string(self, text_content):
         json_objects = self.extract_json_objects(text_content)
         return self.extract_values(json_objects)
 
-    def doit(self, input_string, max_length=10, seed=0, remove_stop_words=False):
+    def doit(self,
+             input_string,
+             max_length=10,
+             min_part_length=14,
+             max_part_length=100,
+             seed=0,
+             remove_stop_words=False):
         random.seed(seed)
         input_string = input_string.lower()
         print("Initial input string: ", input_string)
         values = self.extract_and_process_json_from_string(input_string)
         replacements = [
-            ('_', ' '), ('/', ' and '), ('\\', ' and '), (':', ','),
-            ('"', ','), ('[', ','), (']', ','), ('{', ','), ('}', ','),
-            ('&', ' and '), ('+', ' and '), ('$', 'currency value of '),
-            ('%', ' percent'), ('@', ' at '), ('#', ' number '),
-            ('<', ' less than '), ('>', ' greater than '), ('=', ' equals '),
-            ('*', ''), ('\n', ','),
+            ('_', ' '),
+            ('/', ' and '),
+            ('\\', ' and '),
+            (':', ','),
+            ('"', ','),
+            ('[', ','),
+            (']', ','),
+            ('{', ','),
+            ('}', ','),
+            ('&', ' and '),
+            ('+', ' and '),
+            ('$', 'currency value of '),
+            ('%', ' percent'),
+            ('@', ' at '),
+            ('#', ' number '),
+            ('<', ' less than '),
+            ('>', ' greater than '),
+            ('=', ' equals '),
+            ('*', ''),
+            ('\n', ','),
         ]
 
         def apply_replacements(value):
@@ -509,7 +555,10 @@ class SrlExtractJson:
         values = [apply_replacements(v) for v in values]
         values = [re.sub(r" {2,}", " ", value.strip()) for value in values]
         values = sorted(set(values), key=len, reverse=True)
-        values = sorted(set(itertools.chain.from_iterable(v.split(',') for v in values)), key=len, reverse=True)
+        values = sorted(set(
+            itertools.chain.from_iterable(v.split(',') for v in values)),
+                        key=len,
+                        reverse=True)
         values = [re.sub(r" {2,}", " ", value.strip()) for value in values]
         print("Initial value count: ", len(values))
         print("Extracted values: ")
@@ -527,16 +576,49 @@ class SrlExtractJson:
                 print(f"{i}: {v}")
 
         print("Value count after processing: ", len(values))
-        #sort values
-        values.sort(key=len, reverse=True)
-        #remove duplicate values
-        values = list(dict.fromkeys(values))
-        print("Value count after removing duplicates: ", len(values))
+
         print("Extracted values: ")
         for i, v in enumerate(values, start=1):
             print(f"{i}: {v}")
-        # Randomly select 'max_length' values for final output
-        selected_values = random.sample(values, min(len(values), max_length))
+        #sort values
+        values.sort(key=len, reverse=True)
+        # remove duplicate values
+        values = list(dict.fromkeys(values))
+
+        print("Value count after removing duplicates: ", len(values))
+        # Sort the values by length
+        values.sort(key=len, reverse=False)
+        # filter values based on length and remove "and " from the beginning
+        values = [
+            value[4:] if value.startswith("and ") else value
+            for value in values
+            if min_part_length <= len(value) <= max_part_length
+        ]
+        # Find the midpoint
+        midpoint = len(values) // 2
+
+        # Split the values into short and long
+        short_values = values[:midpoint]
+        long_values = values[midpoint:]
+
+        # Determine the number of values to select from each list
+        num_short = min(len(short_values), int(max_length * 0.7))
+        num_long = min(len(long_values), max_length - num_short)
+
+        # Randomly select values from each list
+        selected_short_values = random.sample(short_values, num_short)
+        selected_long_values = random.sample(long_values, num_long)
+
+        # Combine the selected values
+        selected_values = selected_short_values + selected_long_values
+        #sort values
+        values.sort(key=len, reverse=True)
+        # remove duplicate values
+        values = list(dict.fromkeys(values))
+
+        print(
+            "Value count after removing duplicates and filtering by length: ",
+            len(values))
         print("Selected values: ")
         for i, v in enumerate(selected_values, start=1):
             print(f"{i}: {v}")
@@ -544,10 +626,10 @@ class SrlExtractJson:
         print("Final output string: ", output_string)
         return (output_string, )
 
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-
     "SRL Conditional Interrrupt": SrlConditionalInterrupt,
     "SRL Format String": SrlFormatString,
     "SRL Eval": SrlEval,
